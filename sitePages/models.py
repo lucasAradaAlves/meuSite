e9bf2f5637c3b03fc362623b133de1befc20afaf
Lucas <lucas.arada.alves@usp.br>
2023-11-23 18:17:34 -0300

    Quarto commit - comentários

--- sitePages/models.py
+++ sitePages/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     nomeFilme = models.CharField(max_length=100)
     time  = models.DateTimeField('Data da publicação', null=True, blank=True)
     opiniao = models.CharField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     def __str__(self):
         return self.nomeFilme
     def get_absolute_url(self):
         return reverse('postDetail', args=[str(self.id)])
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    def __str__(self):
+        return f"{self.author.username} - {self.pub_date}"
--- sitePages/views.py
+++ sitePages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
-from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 def mainPage(request):
     context = {}
@@ -10,21 +13,36 @@ def postList(request):
     posts = Post.objects.all()
     return render(request, 'sitePages/postList.html', {'posts': posts})
 
+@login_required
 def postDetail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'sitePages/postDetail.html', {'post': post})
+    comments = post.comments.all().order_by('-pub_date')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.pub_date = timezone.now()
+            comment.save()
+            return redirect('postDetail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'sitePages/postDetail.html', {'post': post, 'comments': comments, 'form': form})
 
 def postEdit(request):
     context = {}
     return render(request, 'sitePages/postEdit.html', context)
 
+@login_required
 def postForm(request):
     if request.method == 'POST':
         formulario = PostForm(request.POST)
         if formulario.is_valid():
-            # Processar os dados do formulário
-            # ...
-            formulario.save()
+            post = formulario.save(commit=False)
+            post.author = request.user
+            post.time = timezone.now()
+            post.save()
             return redirect('postList')
     else:
         formulario = PostForm()
@@ -49,3 +67,33 @@ def postEdit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'sitePages/postEdit.html', {'form': form, 'post': post})
+
+@login_required
+def addPost(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('postList')
+    else:
+        form = PostForm()
+    return render(request, 'sitePages/addPost.html', {'form': form})
+
+@login_required
+def createComment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.pub_date = timezone.now()
+            comment.save()
+            return redirect('postDetail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'sitePages/createComment.html', {'form': form, 'post': post})
