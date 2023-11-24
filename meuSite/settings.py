62257e7410168d3c4be82033053bc7d801a78978
Lucas <lucas.arada.alves@usp.br>
2023-11-24 12:52:44 -0300

    Implementação no Render

--- meuSite/settings.py
+++ meuSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sitePages.apps.SitepagesConfig',
-    'django_select2'
+    'sitePages.apps.SitepagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'meuSite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+    )
 
 
 # Password validation
