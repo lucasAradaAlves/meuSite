2bd447b3b4308eb1d64d072a40d2663e4a8b60b1
Lucas <lucas.arada.alves@usp.br>
2023-11-24 23:52:17 -0300

    Commit 9 - Ajustes settings

--- meuSite/settings.py
+++ meuSite/settings.py
@@ -81,12 +81,6 @@ DATABASES = {
     }
 }
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=600,
-    )
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
